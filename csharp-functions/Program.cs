c4c1088d90ecf3cfaa7c1d90adb73f1b0d59406c
Redux234 <lorenzo.gaetano2002@gmail.com>
2022-11-24 15:50:08 +0100

    seconda funzione

--- csharp-functions/Program.cs
+++ csharp-functions/Program.cs
@@ -1,4 +1,7 @@
-﻿int[] ArrayNumeriInteri = { 1, 5, 6, 9, 1, 2, 3 };
+﻿
+//Funzione stampa array di numeri interi
+
+int[] ArrayNumeriInteri = { 1, 5, 6, 9, 1, 2, 3 };
 SampaArrayNumeriInteri(ArrayNumeriInteri);
 
 
@@ -18,3 +21,53 @@ void SampaArrayNumeriInteri(int[] ArrayNumeriInteri)
     }
     Console.WriteLine(" ]");
 }
+
+//Funzione che eleva al quadrato gli elemnti di un array
+
+int[] ArrayNumeriBase = { 2, 5, 7, 9, 12 };
+Quadrato(ArrayNumeriBase);
+
+
+void Quadrato(int[] ArrayNumeriBase)
+
+    //Stampo l'array base
+{
+
+    Console.Write("[ ");
+    for (int i = 0; i < ArrayNumeriBase.Length; i++)
+    { 
+        if (i == ArrayNumeriBase.Length - 1)
+        {
+            Console.Write(ArrayNumeriBase[i]);
+        }
+        else
+        {
+            Console.Write(ArrayNumeriBase[i] + ", ");
+        }
+    }
+    Console.WriteLine(" ]");
+
+    //Stampo la modifica
+
+    Console.Write("[ ");
+    for(int i=0; i < ArrayNumeriBase.Length; i++)
+    {
+        int nuemroquadrato = ArrayNumeriBase[i] * ArrayNumeriBase[i];
+        if (i == ArrayNumeriBase.Length - 1)
+        {
+            Console.Write(nuemroquadrato);
+        }
+        else
+        {
+            Console.Write(nuemroquadrato + ", ");
+        }
+    }
+    Console.WriteLine(" ]");
+    
+}
+
+//
+
+
+
+
