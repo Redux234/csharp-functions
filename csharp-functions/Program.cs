234a6423f536d0558e0c5495024359c480d1ccbf
Redux234 <lorenzo.gaetano2002@gmail.com>
2022-11-24 15:24:19 +0100

    prima funzione

--- csharp-functions/Program.cs
+++ csharp-functions/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] ArrayNumeriInteri = { 1, 5, 6, 9, 1, 2, 3 };
+SampaArrayNumeriInteri(ArrayNumeriInteri);
+
+
+void SampaArrayNumeriInteri(int[] ArrayNumeriInteri)
+{
+    Console.Write("[ ");
+    for(int i=0; i < ArrayNumeriInteri.Length; i++)
+    {
+        if(i == ArrayNumeriInteri.Length -1)
+        {
+            Console.Write(ArrayNumeriInteri[i]);
+        }
+        else
+        {
+            Console.Write(ArrayNumeriInteri[i] + ", ");
+        }
+    }
+    Console.WriteLine(" ]");
+}
